c08735bb72854e52f2916e890fb9b419ee5808c3
vdresch <dreschv@gmail.com>
2017-06-16 12:03:28 -0300

    Add files via upload

---
+++ Caubói Gaúcho/Caubói Gaúcho/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Caubói Gaúcho
+//
+//  Copyright © 2017 Caubói Gaúcho. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
