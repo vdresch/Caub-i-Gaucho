8a33bf8bcf299eadbbd14f4c4671a10a595449af
vdresch <dreschv@gmail.com>
2017-06-17 12:26:47 -0300

    Update main.c

--- Caubói Gaúcho/Caubói Gaúcho/main.c
+++ Caubói Gaúcho/Caubói Gaúcho/main.c
@@ -7,8 +7,58 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define Tamanho 15
+#define NTerminais 15
+
+
+void le_gramatica(char terminais[Tamanho][NTerminais])
+{
+    FILE* f;
+    
+    int i = 0;
+    int j = 0;
+    char c;
+    
+    f = fopen("sample.txt", "r");
+    if (f == NULL)
+    {
+        printf("Error opening file\n");
+        return;
+    }
+    else
+    {
+        while((c = fgetc(f)) != 10)
+        {
+            ;
+        }
+        while((c = fgetc(f)) != '[')
+        {
+            c = fgetc(f);
+            while((c = fgetc(f)) != ' ')
+            {
+                terminais[i][j] = c;
+                i++;
+            }
+            i = 0;
+            j++;
+            while((c = fgetc(f)) != 10)
+            {
+                ;
+            }
+        }
+        fclose(f);
+        return;
+    }
+}
+
+
+int main(int argc, const char * argv[])
+{
+    
+     char Terminais[Tamanho][NTerminais];
+    
+    
+    
+    
     return 0;
 }
